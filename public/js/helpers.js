1352f3143e36fbb1bcf3f00270b4e1e19cd4a96a
vhrebenko <vhrebenko@gmail.com>
2016-04-28 02:10:29 +0300

    add global object for users local storage

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,3 +1,6 @@
+/**
+ * Global object to compile request
+ */
 var Request = {
   object: {},
   searchQuery: '',
@@ -30,30 +33,73 @@ var Request = {
   }
 };
 
+// Global object storage
+var currentUsers = {};
 
-function loadDataByAjax(url) {
-  getAjaxRequest(url,overloadTableContent);
+/**
+ * Function initial global local storage
+ * @param users object
+ */
+function initialCurrentUsers(users) {
+  if(users.length) {
+    for (var key in users) {
+    var user = users[key];
+    var userId = user['_id'];
+    var userItem = {};
+    userItem['name'] = user['name'];
+    userItem['nickname'] = user['nickname'];
+    userItem['phone'] = user['phone'];
+    userItem['email'] = user['email'];
+    userItem['role'] = user['role'];
+    userItem['gender'] = user['gender'];
+    userItem['vk_link'] = user['vk_link'];
+    userItem['deleted'] = user['deleted'];
+    userItem['last_visit'] = user['last_visit'];
+    userItem['updated_at'] = user['updated_at'];
+    userItem['created_at'] = user['created_at'];
+    currentUsers[userId] = userItem;
+    }
+  }
 }
 
-
-function overloadTableContent(response) {
+/**
+ * Function initial table content
+ * @param response array
+ */
+function initialTableContent(response) {
   if (response.success == true) {
-    $('#table-content').html(prepareContent(response.data.data));
+    initialCurrentUsers(response.data.data);
+    console.log(currentUsers);
+    overloadTableContent();
   }
 }
 
-function prepareContent(users) {
-  if (!users.length) {
-    return '<tr><td colspan="8" style="text-align: center">No Users.</td></tr>';
-  }
+/**
+ * Function overload table content
+ */
+function overloadTableContent() {
+  console.log("overloadTableContent");
+  $('#table-content').html(prepareContent(currentUsers));
+}
 
+/**
+ * Function prepare content in table
+ * @param users object(local storage)
+ */
+function prepareContent(users) {
+  console.log("prepareContent");
+  if(users === undefined) {
+     $('#table-content').html('<tr><td colspan="8" style="text-align: center">No Users.</td></tr>');
+     return false;
+  }
   var url = location.pathname;
   var content = '';
+  var index = 1;
   for (var key in users) {
-    var index = parseInt(key) + 1;
+    /*var index = parseInt(key) + 1;*/
     var user = users[key];
     content += '\
-      <tr>\
+      <tr id="' + key + '">\
         <td>' + index + '</td>\
         <td>' + user['nickname'] + '</td>\
         <td>' + user['name'] + '</td>\
@@ -61,22 +107,48 @@ function prepareContent(users) {
         <td>n/a</td>\
         <td>0</td>\
         <td>\
-          <a class="edit-form-modal" \
-          data-edit-url="' + url + '/' + user['_id'] + '/edit" \
+          <a class="edit-form-modal"\
           href="" >\
             <i class="material-icons">create</i>\
           </a>\
         </td>\
         <td>\
           <a class="delete-form-modal"\
-            data-delete-url="' + url + '/' + user['_id'] + '/destroy" \
             href="">\
               <i class="material-icons">clear</i>\
           </a>\
         </td>\
       </tr>';
+    index++;
   }
   return content;
+}
+
+/**
+ * Function delete current user in local storage
+ * @param userId string
+ */
+function deleteUserInCurrentUsers(userId) {
+  console.log("deleteUserInCurrentUsers");
+  if (userId in currentUsers) {
+  delete currentUsers[userId];
+  } else {
+    console.log("Error global object currentUsers don`t hav current userId!!!");
+  }
+}
+
+/**
+ * Function initial current user in local storage
+ * @param data array
+ * @param userId string
+ */
+function initialUserInCurrentUsers(data, userId) {
+  var itemUser = {};
+  for(var i = 0; i < data.length; i++) {
+    var dataItem = data[i];
+    itemUser[dataItem.name] = dataItem.value;
+  }
+  currentUsers[userId] = itemUser;
 }
 
 // ---------------------- AJAX Requests ------------------------//
@@ -85,12 +157,13 @@ function prepareContent(users) {
  * @param http url var url
  * @param function callback
  */
-function getAjaxRequest(url,callback) {
+function getAjaxRequest(url,callback,userId) {
   $.ajax({
     type: 'get',
     url: url,
     cache: false,
     dataType: 'json',
+    context: userId,
     success : callback
   })
 }
@@ -119,7 +192,7 @@ function postAjaxRequest(url, data, callbackSuccess, callbackError) {
 function pathAjaxRequest(url, data, callback) {
   $.ajax({
     type: 'patch',
-    url: url + "/" + userId,
+    url: url,
     dataType: 'json',
     data: data,
     success: callback
--- public/js/users.js
+++ public/js/users.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    loadDataByAjax(location.pathname);
+    getAjaxRequest(location.pathname, initialTableContent);
 
     $('#search').change(function(){
         var searchPhrase = $(this).val().trim();
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
         Request.prepareSearchQuery();
         Request.updateSearchQuery();
-        loadDataByAjax(Request.searchQuery);
+        getAjaxRequest(Request.searchQuery);
     });
 
     $('.title-sort').click(function(){
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
         Request.prepareSearchQuery();
         Request.updateSearchQuery();
-        loadDataByAjax(Request.searchQuery);
+        getAjaxRequest(Request.searchQuery);
     });
 
     $('#hide_guest').change(function () {
@@ -50,7 +50,262 @@ $(document).ready(function () {
 
         Request.prepareSearchQuery();
         Request.updateSearchQuery();
-        loadDataByAjax(Request.searchQuery);
+        getAjaxRequest(Request.searchQuery);
+    });
+
+    // global variables
+    var $body = $('body');
+    var $name = $('#name');
+    var $nickname = $('#nickname');
+    var $phone = $('#phone');
+    var $email = $('#email');
+
+    var $comments = $('#comments');
+
+    var $roleInput = $(".role input");
+    var $genderInput = $(".gender input");
+
+    var $clubInput = $(".club input");
+    var $baneInput = $(".bane input");
+
+    var $vk_link = $('#vk_link');
+    var $label = $('form label.input-label');
+    var $errorNickname = $('#errorNickname');
+    var $errorPhone = $('#errorPhone');
+    var $errorEmail = $('#errorEmail');
+    var $deleteUserModal = $('#delete-user');
+    var $addEditUserModal = $('#add-user');
+    var $errorMesages = $('.error-mesage');
+    var $buttonAddUser =$('#btn-add-users');
+    var modelState;
+
+    // function event modal window close.
+    function onModalHide() {
+        modelState = "close";
+    }
+
+    // function event modal window open.
+    function onModalShow() {
+        modelState = "open";
+    }
+
+    // function clear fields form
+    function clearFields() {
+        $name.val("");
+        $nickname.val("");
+        $phone.val("");
+        $email.val("");
+        $comments.text("");
+        $errorMesages.text("");
+        $vk_link.val("");
+    }
+
+    // --------------------------------- Functions add user --------------------------//
+
+    /**
+     * @param json var response
+     */
+    function addUser(response) {
+        if (response.success == true) {
+            console.log('done add user');
+            initialCurrentUsers(response.data.data);
+            overloadTableContent();
+            $addEditUserModal.closeModal({
+                complete : onModalHide
+            });
+        }
+    }
+
+    /**
+     * @param object var data
+     */
+    function showErrors(data) {
+        var nickname = $nickname.val();
+        var phone = $phone.val();
+        var email = $email.val();
+        var response = data.responseJSON;
+        if (response.nickname != undefined) {
+            $errorNickname.text( "This nickname  \"" +  nickname + "\"  already exists.");
+        }
+        if (response.phone != undefined) {
+            $errorPhone.text("This phone  \"" +  phone + "\"  already exists.");
+        }
+        if (response.email != undefined) {
+            $errorEmail.text("This email  \"" +  email + "\"  already exists.")
+        }
+        return false;
+    }
+
+    // --------------------------------- Functions delete user --------------------------//
+
+    /**
+     * @param http url var url
+     */
+    function deleteUserRequest(url,userId) {
+        console.log("GET request for delete user");
+        getAjaxRequest(url,deleteUser,userId);
+
+    }
+
+    /**
+     * @param json var response
+     */
+    function deleteUser(response) {
+        if (response.success == true) {
+            deleteUserInCurrentUsers(this);
+            overloadTableContent();
+            $deleteUserModal.closeModal();
+            console.log('done delete user');
+        } else {
+            console.log(response);
+        }
+    }
+
+    // --------------------------------- Functions edit user --------------------------//
+
+    /**
+     * @param json var response
+     */
+    function openModalEditUser(response) {
+        var gender = response.gender;
+        var role = response.role;
+        var vk_link = response.vk_link;
+        var name = response.name;
+        var nickname = response.nickname;
+        var phone = response.phone;
+        var email = response.email;
+
+        /* var comments = response.comments;
+         var club = response.club_id;
+         var bane = response.bane-date;*/
+
+        $addEditUserModal.openModal({
+            complete: onModalHide
+        });
+        onModalShow();
+        $errorMesages.text("");
+        $label.addClass('active');
+        $name.val(name);
+        $nickname.val(nickname);
+        $phone.val(phone);
+        $email.val(email);
+        /*$comments.text(comments);
+         $clubInput.val(club);
+         $baneInput.val(bane);*/
+        $roleInput.val(role);
+        $("#role [value='" + role + "']").attr("selected", "selected");
+        $("#gender [value='" + gender + "']").attr("selected", "selected");
+        /*$("#club_id [value='" + club + "']").attr("selected", "selected");
+         $("#bane-date [value='" + bane + "']").attr("selected", "selected");*/
+        if(response.gender == "f"){
+            gender = "female";
+        } else {
+            gender = "male";
+        }
+        $genderInput.val(gender);
+        if(vk_link != undefined) {
+            $vk_link.val(vk_link);
+        }
+    }
+
+    /**
+     * @param json var response
+     */
+    function editUser(response) {
+        if( response.success == true) {
+            console.log("edit user done");
+            clearFields();
+            $label.removeClass('active');
+            overloadTableContent();
+            $addEditUserModal.closeModal({
+                complete : onModalHide
+            });
+        } else {
+            console.log(response);
+        }
+    }
+
+
+    // --------------------------------- Functions events page-users  --------------------------//
+    $deleteUserModal .closeModal();
+
+    /*The event handler pushing the button delete-users*/
+    $body.on('click', '.delete-form-modal', function (e) {
+        e.preventDefault();
+        $deleteUserModal.openModal();
+        var userId = $(this).parents("tr").attr("id");
+        var url = location.pathname;
+        var userUrl = url + "/" + userId + "/destroy";
+        console.log("start delete user");
+        $('.delete-form').unbind('click');
+        $('.delete-form').click(function (event) {
+            event.preventDefault();
+            deleteUserRequest(userUrl, userId);
+        })
+
+        $('.disagree_delete-form').click(function (event) {
+            event.preventDefault();
+            $deleteUserModal .closeModal();
+        })
+    });
+
+
+    $addEditUserModal.closeModal({
+        complete : onModalHide
+    });
+
+    /*The event handler pushing the button add-users*/
+    $body.on('click', '.add-form-modal', function (e) {
+        e.preventDefault();
+        if(modelState == "open") {
+            return false;
+        }
+        console.log("start add user");
+        var url = $(this).data('create-url');
+        $addEditUserModal.openModal({
+            complete: onModalHide
+        });
+        onModalShow;
+        clearFields();
+
+        $('label[class~=active]').removeClass('active');
+        $('input[class~=valid]').removeClass('valid');
+
+        $('form').unbind('submit');
+        $('form').submit( function(e) {
+            e.preventDefault();
+            console.log("POST request for add user");
+            var self = $(this);
+            var data = self.serializeArray();
+            postAjaxRequest(url, data, addUser, showErrors);
+        })
+    });
+
+
+    /*The event handler pushing the button edit-users*/
+    $body.on('click', '.edit-form-modal', function (e) {
+        e.preventDefault();
+        console.log("start edit user");
+        var userId = $(this).parents("tr").attr("id");
+        var currentItem = {};
+        if(userId in currentUsers) {
+          currentItem = currentUsers[userId];
+        } else {
+          console.log("Error global object currentUsers don`t hav current userId!!!");
+          return false;
+        }
+        openModalEditUser(currentItem);
+        $('form').unbind('submit');
+        $('form').submit(function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            console.log("PATCH request edit user");
+            var self = $(this);
+            var data = self.serializeArray();
+            var url = window.location.pathname + "/" + userId;
+            initialUserInCurrentUsers(self.serializeArray(), userId);
+            pathAjaxRequest(url, data, editUser)
+        });
     });
 
 
