13f7a1499c4ec5588198e3e3844eb269c58ef726
nsmirnova <smirnovanastya1602@gmail.com>
2016-06-03 13:19:05 +0300

    Clubs&Users: make form wider. change some inputs order

--- resources/views/admin/users/form.blade.php
+++ resources/views/admin/users/form.blade.php
@@ -1,92 +1,90 @@
 <div id="add-user" class="modal">
     <div class="modal-content">
-        <div class="container">
-            <div class="row">
-                {!! Form::open( array( 'route' => ['admin.users.store'] )) !!}
+        <div class="row">
+            {!! Form::open( array( 'route' => ['admin.users.store'] )) !!}
 
-                <div class="row ">
-                    <div class="input-field col s6">
-                        {!! Form::label('name', 'Name*', array('class' => 'input-label')) !!}
-                        {!! Form::text('name', NULL, array('required','id' => 'name','type' => 'text','class' => 'validate' . ($errors->has('name')?' invalid':''))) !!}
-                    </div>
-                    <div class="input-field col s6">
-                        {!! Form::label('nickname', 'Nickname*', array('class' => 'input-label')) !!}
-                        {!! Form::text('nickname', NULL, array('autocomplete' => 'on','id' => 'nickname','required','type' => 'text','class' => 'validate' . ($errors->has('nickname')?' invalid':''))) !!}
-
-                    </div>
+            <div class="row ">
+                <div class="input-field col s6">
+                    {!! Form::label('name', 'Name*', array('class' => 'input-label')) !!}
+                    {!! Form::text('name', NULL, array('required','id' => 'name','type' => 'text','class' => 'validate' . ($errors->has('name')?' invalid':''))) !!}
                 </div>
+                <div class="input-field col s6">
+                    {!! Form::label('nickname', 'Nickname*', array('class' => 'input-label')) !!}
+                    {!! Form::text('nickname', NULL, array('autocomplete' => 'on','id' => 'nickname','required','type' => 'text','class' => 'validate' . ($errors->has('nickname')?' invalid':''))) !!}
 
-                <div class="row">
-                    <div class="input-field col s6">
-                        {!! Form::label('phone', 'Phone*',  array('class' => 'input-label')) !!}
-                        {!! Form::text('phone', NULL, array('autocomplete' => 'on','id' => 'phone','required','pattern' => '[0-9]{8,12}','type' => 'tel','class' => 'validate' . ($errors->has('phone')?' invalid':''))) !!}
-
-                    </div>
-                    <div class="input-field col s6">
-                        {!! Form::label('email', 'Email',  array('class' => 'input-label') ) !!}
-                        {!! Form::email('email', NULL, array('autocomplete' => 'on','id' => 'email','type' => 'email','class' => 'validate' . ($errors->has('email')?' invalid':''))) !!}
-
-                    </div>
                 </div>
-
-                <div class="row">
-                    <div class="input-field role col s6">
-                        {!! Form::select(
-                            'role',
-                            array('gamer' => 'gamer', 'host' => 'host', 'admin' => 'admin'),
-                            null,
-                            array('id' => 'role','required')
-                        ) !!}
-                        {!! Form::label('role', 'Role') !!}
-                    </div>
-                    <div class="input-field club col s6">
-                        {!! Form::select(
-                            'club_id',
-                            $clubsForSelect = array( null => "Choose club") + $clubsForSelect,
-                            null,
-                            array('id' => 'club_id', 'class' => 'selectClub')
-                        ) !!}
-                        {!! Form::label('club', 'Club') !!}
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="input-field gender col s6">
-                        {!! Form::select(
-                            'gender',
-                            array('m' => 'male', 'f' => 'female'),
-                            null,
-                            array('id' => 'gender','required')
-                        ) !!}
-                        {!! Form::label('gender', 'Gender') !!}
-                    </div>
-                    <div class="input-field col s6">
-                        {!! Form::label('vk_link', 'VK' , array('class' => 'input-label')) !!}
-                        {!! Form::text(
-                            'vk_link',
-                            NULL,
-                            array('class' => 'validate' . ($errors->has('vk_link')?' invalid':'')
-                        )) !!}
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="bane col s2">
-                        {!! Form::label('bane_date', 'Banned date',  array('class' => 'input-label')) !!}
-                        {!! Form::text('bane_date', null, array('class' => 'validate datepicker' . ($errors->has('date')?' invalid':''))) !!}
-                    </div>
-                    <div class="input-field comments col s10">
-                        {!! Form::label('comments', 'Comments',  array('class' => 'input-label') ) !!}
-                        {{ Form::textarea('comments', null, ['size' => '30x10', 'class' => 'materialize-textarea']) }}
-                    </div>
-                </div>
-
-                @include('errors.list')
-
-                {!! Form::submit('Save', array('class' => 'btn btn-default add-user grey lighten-1')) !!}
-
-                {!! Form::close() !!}
             </div>
+
+            <div class="row">
+                <div class="input-field col s6">
+                    {!! Form::label('phone', 'Phone*',  array('class' => 'input-label')) !!}
+                    {!! Form::text('phone', NULL, array('autocomplete' => 'on','id' => 'phone','required','pattern' => '[0-9]{8,12}','type' => 'tel','class' => 'validate' . ($errors->has('phone')?' invalid':''))) !!}
+
+                </div>
+                <div class="input-field col s6">
+                    {!! Form::label('email', 'Email',  array('class' => 'input-label') ) !!}
+                    {!! Form::email('email', NULL, array('autocomplete' => 'on','id' => 'email','type' => 'email','class' => 'validate' . ($errors->has('email')?' invalid':''))) !!}
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="bane col s4">
+                    {!! Form::label('bane_date', 'Banned date',  array('class' => 'input-label')) !!}
+                    {!! Form::text('bane_date', null, array('class' => 'validate datepicker' . ($errors->has('date')?' invalid':''))) !!}
+                </div>
+                <div class="input-field role col s4">
+                    {!! Form::select(
+                        'role',
+                        array('gamer' => 'gamer', 'host' => 'host', 'admin' => 'admin'),
+                        null,
+                        array('id' => 'role','required')
+                    ) !!}
+                    {!! Form::label('role', 'Role') !!}
+                </div>
+                <div class="input-field club col s4">
+                    {!! Form::select(
+                        'club_id',
+                        $clubsForSelect = array( null => "Choose club") + $clubsForSelect,
+                        null,
+                        array('id' => 'club_id', 'class' => 'selectClub')
+                    ) !!}
+                    {!! Form::label('club', 'Club') !!}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field gender col s6">
+                    {!! Form::select(
+                        'gender',
+                        array('m' => 'male', 'f' => 'female'),
+                        null,
+                        array('id' => 'gender','required')
+                    ) !!}
+                    {!! Form::label('gender', 'Gender') !!}
+                </div>
+                <div class="input-field col s6">
+                    {!! Form::label('vk_link', 'VK' , array('class' => 'input-label')) !!}
+                    {!! Form::text(
+                        'vk_link',
+                        NULL,
+                        array('class' => 'validate' . ($errors->has('vk_link')?' invalid':'')
+                    )) !!}
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field comments">
+                    {!! Form::label('comments', 'Comments',  array('class' => 'input-label') ) !!}
+                    {{ Form::textarea('comments', null, ['size' => '30x10', 'class' => 'materialize-textarea']) }}
+                </div>
+            </div>
+
+            @include('errors.list')
+
+            {!! Form::submit('Save', array('class' => 'btn btn-default add-user grey lighten-1')) !!}
+
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
