1f1a4b901271240ba8df3cf4ee124dc51112417f
vhrebenko <vhrebenko@gmail.com>
2016-06-07 12:15:28 +0300

    users.js: issue #45

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
     $('select').material_select();
-    $(".dropdown-button").dropdown();
+     $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 8, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+    );
     $(".button-collapse").sideNav();
 
     $('.datepicker').pickadate({
